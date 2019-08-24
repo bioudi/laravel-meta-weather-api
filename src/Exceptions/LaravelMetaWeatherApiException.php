8e0b9062d937f1b428022121a0d193f15def3edd
Bioudi Mohamed Amine <bioudiamine@gmail.com>
2019-08-24 18:25:47 +0100

    Handling exceptions

---
+++ src/Exceptions/LaravelMetaWeatherApiException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bioudi\LaravelMetaWeatherApi\Src\Exceptions;
+
+use Exception;
+
+class LaravelMetaWeatherApiException extends \Exception
+{
+    // private $exception;
+
+    // public function __construct($e){
+    //     $this->exception = $e;
+    // }
+
+    // public function render(){
+    //     throw ($this->exception->getMessage());
+    // }
+}
